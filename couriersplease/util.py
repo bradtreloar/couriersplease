58a7e25ad35389cc9599e7aff073b48047041e8b
Brad Treloar <bradley.treloar@gmail.com>
2018-05-17 11:41:45 +0930

    added extra regexp substitutions and take array of field lengths as arg

--- couriersplease/util.py
+++ couriersplease/util.py
@@ -5,37 +5,76 @@ from couriersplease.enum import street_addr_suffix
 
 
 
-def condense_address(addr, maxlength):
+def condense_address(addr, field_lengths):
     # convert address to uppercase to simplify pattern-matching
     addr = addr.upper()
 
     # abbreviate street suffix if address is longer than maxlength
-    if len(addr) > maxlength:
+    if not address_fits_in_fields(addr, field_lengths):
         for suffix, abbr in street_addr_suffix:
             addr = addr.replace(suffix, abbr)
 
-    rules = [
-        # abbreviate "U[NIT] x, y" to "Ux/y"
-        (r"U(NIT ){0,1}(\d+)[,/]{0,1}\s*(\d+)", r"U\2/\3"),
+    substitutions = [
+        # abbreviate "U[NIT][ ]x, y" to "Ux/y"
+        (r"U(NIT){0,1}\s+(\d+)[,/]{0,1}\s*(\d+)", r"U\2/\3"),
 
-        # abbreviate "L[EVEL|VL] x" to "Lx"
-        (r"L(EVEL|VL)+\s{0,1}(\d+),{0,1}", r"LVL\2"),
+        # abbreviate "L[EVEL|VL] x" to "LVLx"
+        (r"L(EVEL|VL)+\s+(\d+),{0,1}", r"LVL\2"),
 
         # abbreviate "BUILDING" to "BLDG"
         (r"BUILDING", r"BLDG"),
 
+        # remove supplemental details in parentheses
+        (r"[\(\[][\w\s]+[\)\]]", r""),
+
         # remove building name but leave level number
-        (r"(LVL{0,1}\d+),{0,1} [\w\s]+,{0,1} (\d+)", r"\1 \2")
+        (r"(LVL{0,1}\d+),{0,1} [\w\s]+,{0,1} (\d+)", r"\1 \2"),
+
+        # remove vowels in middle of words
+        (r"([^ ])[AEIOU]+", r"\1"),
     ]
 
-    # apply rules until addr is reduced to maxlength
-    for rule in rules:
-        # stop applying rules if we're under maxlength
-        if len(addr) <= maxlength:
-            break
+    # apply regexp substitutions until addr is reduced to maxlength
+    for rule in substitutions:
+        # stop applying substitutions if we're under maxlength
+        if address_fits_in_fields(addr, field_lengths):
+            return addr
 
-        # apply the rule
+        # apply the substitution
         addr = re.sub(rule[0], rule[1], addr)
 
     # return the condensed address
     return addr
+
+
+
+def address_fits_in_fields(addr, field_lengths):
+    # split the address into components
+    comps = addr.split()
+
+    comp_index = 0
+    for field_len in field_lengths:
+        # start with no field contents
+        content_len = 0
+
+        # add components to the content length until the field is full or
+        # we run out of components
+        while content_len <= field_len:
+            # add 1 for a space if this is not the start of the contents
+            if content_len > 0:
+                content_len += 1
+
+            try:
+                # add a component
+                content_len += len(comps[comp_index])
+                # move to the next component if we haven't overflowed the field
+                if content_len <= field_len:
+                    comp_index += 1
+            except IndexError:
+                # we used up all the components,
+                # therefore the address fits in the fields
+                return True
+
+    # if we've still got components left over 
+    # then the address doesn't fit in the fields
+    return not comp_index < len(comps)
--- tests/util.py
+++ tests/util.py
@@ -10,15 +10,18 @@ class UtilTestCase(unittest.TestCase):
 
     def test_condense(self):
         addresses = [
-            "U99, 125 O'Sullivan Beach Road",
-            "Unit 99, 125 O'Sullivan Beach Road",
-            "Level 99, Barr Smith Building, 125 O'Sullivan Beach Road",
+            "123 Easy Street",
+            "U 99, 125 Reallylongname Beach Boulevard",
+            "Unit 99, 125 Reallylongname Beach Boulevard",
+            "Level 99, John Smith Building, 125 Reallylongname Beach Boulevard",
+            "Level 99, John Smith Building, 125 Reallylongname Beach Boulevard (deliveries via Thatother Reallylongname Avenue)",
+            "Level 99, John Smith Building, 125 O'Sullivan Beach Boulevard   (deliveries via Thatother Reallylongname Avenue)",
         ]
 
         print()
 
         for addr_string in addresses:
-            cond_addr_string = condense_address(addr_string, 28)
+            cond_addr_string = condense_address(addr_string, [19, 19, 19])
             print(str(len(addr_string)), '=>', str(len(cond_addr_string)))
             print(addr_string, '=>', cond_addr_string)
 
